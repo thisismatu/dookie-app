8ce79bd4efbe64c9a54cc86d8676cda70b998ee7
Mathias Lindholm <mathias.lindholm@reaktor.fi>
2017-02-12 19:43:07 +0200

    Add round button subclass

---
+++ Dookie/RoundedButton.swift
@@ -0,0 +1,22 @@
+//
+//  RoundedButton.swift
+//  Dookie
+//
+//  Created by Mathias Lindholm on 12.02.2017.
+//  Copyright © 2017 Mathias Lindholm. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class RoundButton: UIButton {
+    @IBInspectable var cornerRadius: CGFloat {
+        get {
+            return layer.cornerRadius
+        }
+        set {
+            layer.cornerRadius = newValue
+            layer.masksToBounds = newValue > 0
+        }
+    }
+}
