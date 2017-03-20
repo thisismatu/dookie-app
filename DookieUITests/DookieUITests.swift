58dc439d19590efc8e73403bfbc0818ae45e8c8b
Mathias Lindholm <mathias.lindholm@reaktor.fi>
2017-03-20 14:57:44 +0200

    Init snapsot

---
+++ DookieUITests/DookieUITests.swift
@@ -0,0 +1,33 @@
+//
+//  DookieUITests.swift
+//  DookieUITests
+//
+//  Created by Mathias Lindholm on 20.03.2017.
+//  Copyright © 2017 Mathias Lindholm. All rights reserved.
+//
+
+import XCTest
+
+class DookieUITests: XCTestCase {
+
+    override func setUp() {
+        super.setUp()
+        UIPasteboard.general.string = "-KdbklMPTqReQ-OFr-v7"
+        let app = XCUIApplication()
+        setupSnapshot(app)
+        app.launch()
+    }
+
+    func testScreenshots() {
+        let app = XCUIApplication()
+        snapshot("01LoginScreen")
+        app.buttons["Join a shared pet"].tap()
+        app.buttons["Join"].tap()
+        snapshot("02MainScreen")
+        app.navigationBars["Dookie"].buttons["ic cog"].tap()
+        snapshot("03SettingsScreen")
+        app.navigationBars["Dookie.SettingsView"].buttons["ic ellipsis"].tap()
+        app.sheets.buttons["Leave Dookie"].tap()
+    }
+
+}
